213be819caf8376768468ed42081c0a8ef9e86e5
Marcos Felipe Rocha <marcos.fellps@gmail.com>
2023-06-04 18:10:18 -0300

    Add project files.

---
+++ Data/Class.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EstoqueWebApp.Data;
+public class ApplicationDbContext : DbContext
+{
+   
+    /*
+    public DbSet<Card> Cards { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+    */
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+    {
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+
+string ConnectionString = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=root";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(ConnectionString)
+);
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
